be9875f4087d41dfb0e5a6200cde27dc61bc5126
baryshevandriy <baryshev.andriy@yandex.ru>
2022-10-10 01:10:24 +0300

    Updates

---
+++ precache-manifest.7560e9d55c15af97376c5ca773515da2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ef47dc86e1b7f302eb5",
+    "url": "/formReact/static/css/main.78913aa9.chunk.css"
+  },
+  {
+    "revision": "2ef47dc86e1b7f302eb5",
+    "url": "/formReact/static/js/main.2ef47dc8.chunk.js"
+  },
+  {
+    "revision": "8430a659ad8e53b656c8",
+    "url": "/formReact/static/js/1.8430a659.chunk.js"
+  },
+  {
+    "revision": "823882ffe6d4c5b9fa20",
+    "url": "/formReact/static/js/runtime~main.823882ff.js"
+  },
+  {
+    "revision": "5d0c84549944d3f3f73c8609f6fb51a6",
+    "url": "/formReact/index.html"
+  }
+];
